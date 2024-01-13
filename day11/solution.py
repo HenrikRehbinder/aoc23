57b1b7367c6c3c35cc66e16b6c5092806b3a4d67
henrikrehbinder <henrik.rehbinder@gmail.com>
2024-01-13 17:10:32 +0100

    done and cleaned

--- day11/solution.py
+++ day11/solution.py
@@ -1,43 +1,55 @@
 import sys
-sys.path.append('..')
+#sys.path.append('..')
 from utils.imports import *
-file = 'test1.txt'
+file = 'input.txt'
 with open(file) as file:
-    data = [s.strip() for s in file.readlines()]
+    data_org = [s.strip() for s in file.readlines()]
+
+data = [[ddd for ddd in dd] for dd in data_org]
+
+n_rows = len(data)
+n_cols = len(data[0])
+
+stars = np.argwhere(np.array(data) == '#')
+
+star_rows = sorted(list(set(stars[:, 0])))
+star_cols = sorted(list(set(stars[:, 1])))
+
+non_star_rows = [row for row in range(n_rows) if not (row in star_rows)]
+non_star_cols = [col for col in range(n_cols) if not (col in star_cols)]
 
 
-# expand universe
-dots = ''.join(['.' for i in range(len(data[0]))])
-exp_data = []
-for i, d in enumerate(data):
-    exp_data.append(d)
-    if d == dots:
-        exp_data.append(d)
+def distance2(star_1, star_2, n, non_star_rows, non_star_cols):
+    def ax_dist(star_1, star_2, axis, non):
+        a_start = min((star_1[axis], star_2[axis]))
+        a_end = max((star_1[axis], star_2[axis]))
+        a_dist = 0
+        for ind in range(a_start + 1, a_end + 1):
+            if ind in non:
+                a_dist += n
+            else:
+                a_dist += 1
+        return a_dist
+
+    return (
+            ax_dist(star_1, star_2, axis=0, non=non_star_rows) +
+            ax_dist(star_1, star_2, axis=1, non=non_star_cols)
+    )
 
 
-# find # positions.
-def find_stars(d):
-    if d.find('#') != -1:
-        return [m.start() for m in re.finditer('#', d)]
-    else:
-        return None
+print((distance2((5,1), (9, 4), n=2, non_star_rows=non_star_rows, non_star_cols=non_star_cols), 9))
+print((distance2((9, 0), (9, 4), n=2, non_star_rows=non_star_rows, non_star_cols=non_star_cols), 5))
 
 
-stars = []
-for row, d in enumerate(exp_data):
-#    print(d)
-    cols = find_stars(d)
-    if cols != None:
-        for col in cols:
-            stars.append([row, col])
-#    print(cols)
-måste expandera rader också. 
-
-def distance(star_1, star_2):
-    # inte verifierad, 1an är adhoc
-    return(abs(star_1[0] - star_2[0]) + abs(star_1[1] - star_2[1])+1)
+def acc_distances(stars, n, non_star_rows, non_star_cols):
+    dists = []
+    for i in range(len(stars)-1):
+        for j in range(len(stars)-i-1):
+            dists.append(distance2(
+                stars[i], stars[j+i+1], n=n, non_star_rows=non_star_rows, non_star_cols=non_star_cols)
+            )
+    return sum(dists)
 
 
-print(distance(stars[0], stars[6]))
-print(distance(stars[2], stars[5]))
-print(distance(stars[7], stars[8]))
+print(f'Answ1: {acc_distances(stars, 2, non_star_rows, non_star_cols)}, (9627977 is correct)')
+print(f'Answ2: {acc_distances(stars, 1000000, non_star_rows, non_star_cols)}, (644248339497 is correct)')
