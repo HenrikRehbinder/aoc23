c868672d5833b677f3bf401f7d2d8386d4ed4077
henrikrehbinder <henrik.rehbinder@gmail.com>
2024-02-12 20:36:44 +0100

    .

--- day16/solution.py
+++ day16/solution.py
@@ -19,8 +19,8 @@ beams = [{
 class Mirrors():
     def __init__(self, mirrors):
         self.mirrors = mirrors
-        self.beam_visits = np.array([['.' for d in dat] for dat in data])
-        self.beam_directions = {
+        self.visits = np.array([['.' for d in dat] for dat in data])
+        self.directions = {
             'left': np.array([[False for d in dat] for dat in data]),
             'right': np.array([[False for d in dat] for dat in data]),
             'up': np.array([[False for d in dat] for dat in data]),
@@ -31,7 +31,7 @@ class Mirrors():
 
     def add_beam(self, start_pos, start_dir):
         beam = Beam(
-            start_pos, start_dir, self.mirrors, self.beam_visits, self.beam_directions
+            start_pos, start_dir, self#.mirrors, self.beam_visits, self.beam_directions
             )
         self.beams.append(beam)
 
@@ -40,24 +40,37 @@ class Mirrors():
         self.add_beam(start_pos, start_dir)
         done = False
 #        while (not done) and (len(self.beams)<10):
-        i = 0
-        while i<10:
-            done = True
-            beam = self.beams[i]
+        #i = 0
+        while not done: 
+            #i<10:
+            #print(i)
+            #print(len(self.beams))
+            print('----')
+            #done = True
+            nsb = []
+            for j, b in enumerate(self.beams):
+                if b.status == 'not_started':
+                    nsb.append(j)
+            print(nsb)
+            if len(nsb)==0:
+                done = True
+            else:
+                beam = self.beams[nsb[0]]
+                print('hello')
+                print(beam)
+#            non_started_beams = [i for i,b in enumerateself.beams[i]
             #for beam in self.beams:
-
-            if beam.status == 'not_started':
-                done = False
-                beam_visits, beam_directions, beam_splits, status = beam.evolve()
-                self.beam_visits = beam_visits
-                self.beam_directions = beam_directions
+                beam_splits, status = beam.evolve()
+                #self.visits = beam_visits
+                #self.beam_directions = beam_directions
                 print(status)
                 #print(self.beam_visits)
                 for beam_split in beam_splits:
                     #Kolla om den är i en loop. Eller kollar man det innan man skapar childrn. 
                     self.add_beam(beam_split[0], beam_split[1])
-                i += 1
-                print(('len(self.beams)', len(self.beams), i))
+            #    i += 1
+
+               # print(('len(self.beams)', len(self.beams))
 
 
        # self.beam_visits = beam.visits
@@ -71,7 +84,7 @@ class Mirrors():
 
 
 class Beam():
-    def __init__(self, start_pos, start_dir, mirrors, beam_visits, beam_directions):
+    def __init__(self, start_pos, start_dir, mirrors):#, beam_visits, beam_directions):
         self.start_pos = start_pos
         self.start_dir = start_dir
         self.status = 'not_started' # 'not_started', 'split???', 'terminated'
@@ -79,8 +92,8 @@ class Beam():
         self.directions = [start_dir]
         self.children = []
         self.mirrors = mirrors
-        self.visits = beam_visits
-        self.visit_directions = beam_directions
+        #self.visits = beam_visits
+        #self.visit = beam_directions
         self.reflections = {
             '/': {
                 'left': 'down',
@@ -121,7 +134,7 @@ class Beam():
             self.take_step()
         self.trajectory = self.trajectory#[1:] #look out. to remove first out of grid pos. will it fly for child beams
         self.directions = self.directions#[1:]
-        return self.visits, self.visit_directions, self.children, self.status
+        return self.children, self.status
 
     def take_step(self):
         # position
@@ -129,6 +142,8 @@ class Beam():
         old_direc = self.directions[-1]
         #print(('dir', direc))
         old_pos = self.trajectory[-1]
+        self.mirrors.visits[old_pos[0], old_pos[1]] = '#'
+ 
         if old_direc == 'left':
             new_pos = [old_pos[0], old_pos[1] - 1]
         elif old_direc == 'right':
@@ -138,108 +153,53 @@ class Beam():
         else:
             new_pos = [old_pos[0] + 1, old_pos[1]]
         print(new_pos)
+        escape = (
+                (min(new_pos) == -1) or
+                (new_pos[0] == self.mirrors.mirrors.shape[0]) or
+                (new_pos[1] == self.mirrors.mirrors.shape[1])
+                )
+        if escape:
+            self.status = 'terminated'
+        else:
+
         #direction
-        mirror = self.mirrors[new_pos[0], new_pos[1]]
+            mirror = self.mirrors.mirrors[new_pos[0], new_pos[1]]
         #print(mirror)
         #print(new_pos)
-        new_dir = self.reflections[mirror][old_direc]
+            new_dir = self.reflections[mirror][old_direc]
         #print(('new_pos',new_pos))
         #print(min(new_pos))
         #TODO: lösing på gång. Kolla ny(a) rikting(ar) här.
-        is_split = (new_dir == 'split')
-        if is_split:
-            if old_direc in ['left', 'right']:
-                new_dirs = ['down', 'up']
-            else:
-                new_dirs = ['left', 'right']
-        else:
-            new_dirs = [new_dir]
-
-        for new_dir in new_dirs:
-            loop = (
-                self.visits[new_pos[0], new_pos[1]] == '#' and
-                self.visit_directions[new_dir][new_pos[0], new_pos[1]] is True
-            )
-            escape = (
-                (min(new_pos) == -1) or
-                (new_pos[0] == self.mirrors.shape[0]) or
-                (new_pos[1] == self.mirrors.shape[1])
-                )
-            if not (loop or escape):
-                if is_split:
-                    self.visits[new_pos[0], new_pos[1]] = '#'
-                    self.children.append([new_pos, new_dir])
-                    self.status = 'terminated' 
+            is_split = (new_dir == 'split')
+            if is_split:
+                if old_direc in ['left', 'right']:
+                    new_dirs = ['down', 'up']
                 else:
-                    self.visits[new_pos[0], new_pos[1]] = '#'
-                    self.visit_directions[old_direc][new_pos[0], new_pos[1]] = True
-                    self.trajectory.append(new_pos)
+                    new_dirs = ['left', 'right']
             else:
-                self.status = 'terminated'
+                new_dirs = [new_dir]
 
-    def print_visits(self):
-            for visit_row in self.visits:
-                print(''.join([v for v in visit_row]))
+            for new_dir in new_dirs:
+                loop = (
+                    self.mirrors.visits[new_pos[0], new_pos[1]] == '#' and
+                    self.mirrors.directions[new_dir][new_pos[0], new_pos[1]] is True
+                )
+                if not loop:
+                    if is_split:
+                        #self.mirrors.visits[new_pos[0], new_pos[1]] = '#'
+                        self.children.append([new_pos, new_dir])
+                        self.status = 'terminated'
+                    else:
+                        #self.mirrors.visits[new_pos[0], new_pos[1]] = '#'
+                        self.mirrors.directions[old_direc][new_pos[0], new_pos[1]] = True
+                        self.directions.append(new_dir)
+                        self.trajectory.append(new_pos)
+                else:
+                    self.status = 'terminated'
+
+#    def print_visits(self):
+#            for visit_row in self.visits:
+#                print(''.join([v for v in visit_row]))
 
 mirrors = Mirrors(data)
 mirrors.do([0,0], 'right')
-if False:
-    mirrors.add_beam([0,-1], 'right')
-    bv, bd, bs, st = mirrors.beams[0].evolve()
-    for bss in bs: mirrors.add_beam(bss[0], bss[1])
-    for beam in mirrors.beams:
-        print(beam.status)
-
-
-
-'''
-        else:
-            
-            print(new_dir)
-            print(self.visit_directions[new_dir[0]][new_pos[0], new_pos[1]])
-            print(self.visit_directions[new_dir[1]][new_pos[0], new_pos[1]])
-            loop = (
-                self.visits[new_pos[0], new_pos[1]] == '#' and (
-                    (self.visit_directions[new_dir[0]][new_pos[0], new_pos[1]] is True) or
-                    (self.visit_directions[new_dir[1]][new_pos[0], new_pos[1]] is True)
-                    )
-                    )
-        if (
-            (min(new_pos) == -1) or
-            new_pos[0] == self.mirrors.shape[0] or
-            new_pos[1] == self.mirrors.shape[1]
-            ):
-            #print(new_pos)
-#            print('escaped')
-            self.status = 'terminated'
-        elif (
-            self.visits[new_pos[0], new_pos[1]] == '#' and
-            self.visit_directions[old_direc][new_pos[0], new_pos[1]] is True
-            #TODO: Här är nog problemet. Vid riktningsbyte (nedan) så reggas den 
-            #nya riktingen. Här kollas ingående riktning. Det är därför vi inte
-            # upptäcker looparna. Speciellt inte vid split. 
-            ):
- #           print('terminated_loop')
-            print(f'loop detected {new_pos[0], new_pos[1]}')
-            print((self.visits[new_pos[0], new_pos[1]], old_direc)) #TODO Ska det vara new_dir här?
-            #print((direc, self.visit_directions[direc][new_pos[0], new_pos[1]]))
-            self.status = 'terminated'
-        else:
-            self.visits[new_pos[0], new_pos[1]] = '#'
-            self.visit_directions[old_direc][new_pos[0], new_pos[1]] = True
-            self.trajectory.append(new_pos)
-
-
-            if new_dir != 'split':
-                self.directions.append(new_dir)
-            else:
- #               print('split')
-                self.status = 'terminated'
-
-                if old_direc in ['left', 'right']:
-                    self.children.append([new_pos, 'up'])
-                    self.children.append([new_pos, 'down'])
-                else:
-                    self.children.append([new_pos, 'left'])
-                    self.children.append([new_pos, 'right'])
-'''
