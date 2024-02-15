86e869b024d371214b7bafc5465ad0b9d7fb7fd1
henrikrehbinder <henrik.rehbinder@gmail.com>
2024-02-15 20:38:03 +0100

    squash .
    .
    
    Revert "."
    
    This reverts commit 035974662a56b496ad5309045020a6562b24ef3d.
    
    Revert "."
    
    This reverts commit 1070a208ac661369fb167ff00c0e5c96b764b12b.
    
    Revert "."
    
    This reverts commit ca14bb53f795fef194616012b111cb5be2a9c77c.
    
    .
    
    .
    
    .
    
    cleaned

--- day16/solution.py
+++ day16/solution.py
@@ -63,8 +63,8 @@ class Mirrors():
                 'down': 'down'
                   }
         }
-
     
+
     def print_visits(self):
         for v in self.visits:
             print(''.join(v))
@@ -93,10 +93,10 @@ class Mirrors():
                 self.add_beam(sp, new_dir)
         else:
             self.add_beam(sp, sd)
-               
+
         done = False
         i = 0
-        while not done: 
+        while not done:
             nsb = []
             for j, b in enumerate(self.beams):
                 if not b.terminated:
@@ -111,22 +111,8 @@ class Mirrors():
                 beam_splits, _ = beam.evolve()
                 for beam_split in beam_splits:
                     print('adding splits')
-                    #Kolla om den är i en loop. Eller kollar man det innan man skapar childrn. 
                     self.add_beam(beam_split[0], beam_split[1])
                     i += 1
-                #if i > 40: 
-                #    print('debug break')
-                #    done=True
-               # print(('len(self.beams)', len(self.beams))
-
-
-       # self.beam_visits = beam.visits
-       # print(beam.visits)
-       # self.beam_directions = beam.visit_directions#
-
-#        while not beam.status == 'terminated':
-#            for child in beam.children:
-#                self.add_beam(child.start_pos, child.start_dir)
 
 
 def step(old_dir, old_pos):
@@ -142,29 +128,25 @@ def step(old_dir, old_pos):
 
 
 class Beam():
-    def __init__(self, start_pos, start_dir, mirrors):#, beam_visits, beam_directions):
+    def __init__(self, start_pos, start_dir, mirrors):
         self.start_pos = start_pos
         self.start_dir = start_dir
-        self.terminated = False # 'not_started', 'split???', 'terminated'
+        self.terminated = False 
         self.trajectory = [start_pos]
         self.directions = [start_dir]
         self.children = []
         self.mirrors = mirrors
-        #self.visits = beam_visits
-        #self.visit = beam_directions
-                #if self.status == 'not_started':
-        #    self.evolve()
 
 
     def evolve(self):
         j = 0
-        while (not self.terminated) and (j<20):
+        while (not self.terminated):
             self.take_step()
             print(j)
             j += 1
         print('EVOLVED')
-        self.trajectory = self.trajectory#[1:] #look out. to remove first out of grid pos. will it fly for child beams
-        self.directions = self.directions#[1:]
+        self.trajectory = self.trajectory
+        self.directions = self.directions
         return self.children, self.terminated
 
 
@@ -173,23 +155,12 @@ class Beam():
         old_dir = self.directions[-1]
         self.mirrors.visits[old_pos[0], old_pos[1]] = '#'
         self.mirrors.directions[old_dir][old_pos[0], old_pos[1]] = True
-
         new_pos = step(old_dir, old_pos)
-#        if old_dir == 'left':
-#            new_pos = [old_pos[0], old_pos[1] - 1]
-#        elif old_dir == 'right':
-#            new_pos = [old_pos[0], old_pos[1] + 1]
-#        elif old_dir == 'up':
-#            new_pos = [old_pos[0] - 1, old_pos[1]]
-#        else:
-#            new_pos = [old_pos[0] + 1, old_pos[1]]
-        #print(new_pos)
         escape = (
                 (min(new_pos) == -1) or
                 (new_pos[0] == self.mirrors.mirrors.shape[0]) or
                 (new_pos[1] == self.mirrors.mirrors.shape[1])
                 )
-        
         if escape:
             print(f'escaped at {new_pos}')
             self.terminated = True
@@ -230,28 +201,15 @@ class Beam():
                 self.directions.append(new_dir)
                 self.trajectory.append(new_pos)
 
-#    def print_visits(self):
-#            for visit_row in self.visits:
-#                print(''.join([v for v in visit_row]))
 
 mirrors = Mirrors(data)
-if file_name == 'test1.txt':
-    mirrors.do([0,-1], 'right')
-else:
-    mirrors.do([0,-1], 'right')
+mirrors.do([0,-1], 'right')
+
 
 print(f'Ans1: {mirrors.ans1()} - 7046 is the right answer')
 
-'''
-Det här ska inte vara så svårt. Jag måste lösa att starta en beam 
-utifrån och kunna hantera att det första som händer är split. 
-Det enklaste är nog att 
-1) Lyft upp reflections till mirrors. Den passar ändå bättre där. 
-2. Gör ett litet fulhack för att hantera split i do()
-'''
 
 n_rows, n_cols = mirrors.mirrors.shape
-
 best = 0
 for row in range(n_rows):
     print(f'row {row}/{n_rows}')
