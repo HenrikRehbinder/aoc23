da9d8451ad5a9ddbaddd47df2d37c21d7a5c621c
henrikrehbinder <henrik.rehbinder@gmail.com>
2024-02-23 16:41:32 +0100

    .

--- day17/solution.py
+++ day17/solution.py
@@ -34,15 +34,19 @@ def new_states(x, blocks):
     new = []
     # Start criteria, we know that num_moves=0 here
     if np.all(x['dir'] == np.array([0, 0])):
+        #print('*')
         for dir in np.array([[1, 0], [0, 1],[-1, 0], [0, -1]]):
             m = move(x['pos'], dir)
+        #    print(m)
             if in_blocks(m, blocks):
+         #       print('***')
                 new.append({
                     'pos': m,
                     'dir': dir,
                     'num_moves': x['num_moves']+1
                     })
     else:
+        #print('**')
         if x['num_moves'] < 3:
             for dir, t in zip([x['dir']] + turns(x['dir']), [False, True, True]):
                 #print((dir, x['dir']))
@@ -70,22 +74,22 @@ def new_states(x, blocks):
     return new
 
 
-
 def terminated(x, blocks):
-    return np.all(x['pos'] == np.array([blocks['n_rows']-1, blocks['n_cols']-1]))
+    return V[x['pos'][0], x['pos'][1]] is not None
+    #return np.all(x['pos'] == np.array([blocks['n_rows']-1, blocks['n_cols']-1]))
 
 
 def cost(x, blocks):
     return blocks['cost'][x['pos'][0], x['pos'][1]]
 
 
-def V_fun(x, blocks, indent):
+def V_fun_old(x, blocks, indent):
     #....
     print(indent + 'entering V with ')
     print(f'{indent}{x}')
     if terminated(x, blocks):
         print(f'{indent}terminated, cost {cost(x, blocks)}')
-        return cost(x, blocks)
+        return V[x['pos'][0], x['pos'][1]]
     else:
         
         n_states = new_states(x, blocks) 
@@ -98,36 +102,108 @@ def V_fun(x, blocks, indent):
             costs.append(this_cost + V_fun(n_state, blocks, indent+' '))
             i += 1
         print(indent+f'exiting V, cost {min(costs)}')
+        V[x['pos'][0], x['pos'][1]] = min(costs)
         return min(costs)
 
-print('SMALLER')
-n = 3
-data_arr = np.array(data_arr)[:n, :n]
-# action a: en förflyttning (alltså en ny pos)
-# state x: koordinater, riktning, num_of_moves (antal förflyttningar i en rikting)
-tmp = [None for col in range(data_arr.shape[1])]
-V = [tmp for row in range(data_arr.shape[0])]
-V = np.array(V)
-V[-1,-1] = data_arr[-1,-1]
+def V_fun(x, blocks, indent, V_bound, acc_cost, start_state):
+    #....
+    n_states = new_states(x, blocks) 
+    #print(f'{indent}num of new_states: {len(n_states)}')
+    this_cost = cost(x, blocks)
+    i = 0
+    for n_state in n_states: 
+     #   print(f'{indent}{(i, n_state)}')
+        if terminated(n_state, blocks):
+            new_acc_cost = acc_cost + V[n_state['pos'][0], n_state['pos'][1]]
+            #if new_acc_cost < V_bound:
+            V[start_state['pos'][0], start_state['pos'][1]] = min(
+                V[start_state['pos'][0], start_state['pos'][1]], new_acc_cost
+            )
+        else:
+            new_acc_cost = acc_cost + cost(n_state, blocks)
+            if new_acc_cost >= V[start_state['pos'][0], start_state['pos'][1]]:
+                pass
+            else:
+                V_fun(n_state, blocks, indent+' ', 
+                      V_bound, new_acc_cost, start_state)
 
+# Börja med rad/kolonn närmast slutet 
+# Ta ett steg i varje riktning och håll inkrementera kostnaden. När en av dem terminerar
+    # har vi en max(V(state))kan vi stänga ner de andra om de får en kostnad högre 
+    
+# Jag kan inte bygga upp det nerifrån höger (eftersom det kan finnas bättre lösningar när problemet blir större)
+# En idé är att hålla koll på ackumulerad kostnad när man traverserar.
+# Det här ska ju bara vara att göra - vad gör jag för fel?!
+# Det är ju out of the box dynp. Det måste vara fel i V_fun 
+print('SMALLER')
+n = 8
+data_arr = np.array(data_arr)[:n, :n]
 blocks = {
     'cost': data_arr,
     'n_rows': data_arr.shape[0],
     'n_cols': data_arr.shape[1],
-    'V': V
 }
-initial_state = {
-    'pos': np.array([0, 0]),
-    'dir': np.array([0, 0]),
-    'num_moves': 0
-}
+# action a: en förflyttning (alltså en ny pos)
+# state x: koordinater, riktning, num_of_moves (antal förflyttningar i en rikting)
+V_b = sum(sum(blocks['cost']))
+#tmp = [None for col in range(data_arr.shape[1])]
+tmp = [V_b for col in range(data_arr.shape[1])]
+V = [tmp for row in range(data_arr.shape[0])]
+V = np.array(V)
+V[-1, -1] = data_arr[-1,-1]
+print('Initial V')
+print(V)
+#V[2, 1] = 5
+#V[1, 2] = 6
+#V[1, 1] = 7
+n_rows = blocks['n_rows']
+#for rc, l in zip(reversed(range(blocks['n_rows']-1)), range(2, blocks['n_rows']+1)):
+   
+for rc in reversed(range(n_rows-1)):
+    print(f'rc: {rc}')
 
-ans1= V_fun(initial_state, blocks, '') - cost(initial_state, blocks)
+    #print(f'l: {l}')   
+    for j in range(rc+1, n_rows):
+        print(f'j:{j}')
+        print((rc,j))
+        initial_state = {
+            'pos': np.array([rc, j]),
+            'dir': np.array([0, 0]),
+            'num_moves': 0
+            }
+        print(initial_state['pos'])
+        V_fun(initial_state, blocks, '', None, cost(initial_state, blocks), initial_state)
+        print(V)
+        print((j,rc))
+        initial_state = {
+            'pos': np.array([j, rc]),
+            'dir': np.array([0, 0]),
+            'num_moves': 0
+            }
+        print(initial_state['pos'])
+        V_fun(initial_state, blocks, '', None, cost(initial_state, blocks), initial_state)
+        print(V)
+    print((rc,rc))
+    initial_state = {
+       'pos': np.array([rc, rc]),
+        'dir': np.array([0, 0]),
+        'num_moves': 0
+        }
+    print(initial_state['pos'])
+    V_fun(initial_state, blocks, '', None, cost(initial_state, blocks), initial_state)
+    print(V)
 
 #ans1= V(initial_state, blocks)
-
-print(f'Ans1: {ans1}')
-
+print(V)
+print(blocks['cost'])
+#print(f'Ans1: {ans1}')
+initial_state = {
+    'pos': np.array([2, 2]),
+    'dir': np.array([0, 0]),
+    'num_moves': 0
+    }
+print('Fan. Dels är svaret för högt. Dels så har jag missat något fundamentalt. Jag kan inte bara landa i en evakluerad V eftersom jag måste ta hänsyn till att jag kanske redan kört för långt i samma rikting. ')
+print('Om optimal path från en V börjar med tex tre raka så måste jag svänga in i den')
 
 
 if False:
