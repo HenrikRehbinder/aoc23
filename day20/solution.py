e7eb0cc5f7f08856a158a112676dd90ed286ab45
henrikrehbinder <henrik.rehbinder@gmail.com>
2024-05-05 22:12:18 +0200

    Här har jag send i modulerna. Planerar att hantera send i en kö istället. committar detta ifall jag måste backa.

--- day20/solution.py
+++ day20/solution.py
@@ -1,115 +1,157 @@
 import sys
 sys.path.append('..')
 from utils.imports import *
-file = 'test2.txt'
+file = 'test1.txt'
 with open(file) as file:
     data = [s.strip() for s in file.readlines()]
 
 
-class Module:
-    def __init__(self, str):
-        typename, outputs = str.replace(' ', '').split('->')
-        if typename[0] == 'b':
-            self.type = 'bc'
-            self.name = typename
-            self.state = None
-            self.last_received = None
-            #self.will_send = True
-        elif typename[0] == '%':
-            self.type = 'ff'
-            self.name = typename[1:]
-            self.state = 'off'
+class FlipFlop:
+    def __init__(self, name, output):
+        self.name = name
+        self.state = 'off'
         #    self.last_received = []
             #self.will_send = False
-        elif typename[0] == '&':
-            self.type = 'cj'
-            self.name = typename[1:]
-            self.state = None
-        self.outputs = outputs.split(',')
- #       self.inputs = [] # needed?
-        self.last_inputs = {}
+        self.output = output
+#        self.last_inputs = {}
         self.last_input = None
+        self.input_modules = []
 
-    def add_input(self, input):
-  #      self.inputs.append(input) # needed?
-        self.last_inputs[input] = 'low'
-        #self.last_input = input
+    def add_input_module(self, input):
+        self.input_modules.append(input)
+
+    def get_outputs(self):
+        return [self.output]
 
     def process(self, input, pulse):
         # pulses  = dict(input, pulse)
         self.last_input = input
-        if self.type == 'bc':
-            self.last_inputs[input] = pulse
+        if pulse == 'low':
+            if self.state == 'off':
+                self.state = 'on'
+            elif self.state == 'on':
+                self.state = 'off'
+            else:
+                print('error')
 
-        elif self.type == 'ff':
-            self.last_inputs[input] = pulse
-            if pulse == 'low':
-                if self.state == 'off':
-                    self.state = 'on'
-                elif self.state == 'on':
-                    self.state = 'off'
-                else:
-                    print('error')
-        elif self.type == 'cj':
-            self.last_inputs[input] = pulse
-        elif self.type == 'output':
+    def send(self):
+        if self.last_input == 'high':
             pass
         else:
-            print('Error')
+            #print('state: '+self.state)
+            if self.state == 'on':
+                to_send = 'high'
+            else:
+                to_send = 'low'
+            num_of_pulses[to_send] += 1
+            modules[self.output].process(self.name, to_send)
+            modules[self.output].send()
+
+
+class Conjunction:
+    def __init__(self, name, output):
+        #typename, outputs = str.replace(' ', '').split('->')
+        self.name = name
+        self.output = output
+        self.input_modules = []
+        self.last_inputs = {}
+    def add_input_module(self, input):
+        self.input_modules.append(input)
+        self.last_inputs[input] = 'low'
+
+    def get_outputs(self):
+        return [self.output]
+
+    def process(self, input, pulse):
+        # pulses  = dict(input, pulse)
+        #self.last_input = input
+        self.last_inputs[input] = pulse
 
     def send(self):
         #print(f'In {self.name}, to send *{self.to_send}* to {self.outputs} {self.will_send}')
-        if self.type == 'bc':
-            to_send = self.last_inputs[self.last_input]
-        elif self.type == 'cj':
-            to_send = 'low'
-            for i, p in self.last_inputs.items():
-                if p == 'low':
-                    to_send = 'high'
-                    break
-        elif self.type == 'ff':
-            #ff
-            if self.last_inputs[self.last_input] == 'high':
-                to_send = None
-            else:
-                #print('state: '+self.state)
-                if self.state == 'on':
-                    to_send = 'high'
-                else:
-                    to_send = 'low'
-        else:
-            to_send = None
-        if to_send is not None:
-            for output in self.outputs:
-                print('send:' + self.name+' -'+to_send+'-> '+output)
-                modules[output].process(self.name, to_send)
-            for output in self.outputs:
-                num_of_pulses[to_send] += 1
-                modules[output].send()
-            #for output in self.outputs:
-             #   modules[output].send()
+        to_send = 'low'
+        for i, p in self.last_inputs.items():
+            if p == 'low':
+                to_send = 'high'
+                break
+        num_of_pulses[to_send] += 1
+        modules[self.output].process(self.name, to_send)
+        modules[self.output].send()
+
+class Broadcast:
+    def __init__(self, name, outputs):
+        # typename, outputs = str.replace(' ', '').split('->')
+        self.name = name
+        self.outputs = outputs
+        self.input_modules = []
+        self.last_input = None
+
+
+    def add_input_module(self, input):
+        self.input_modules.append(input)
+        #self.last_input = ''
+
+    def get_outputs(self):
+        return self.outputs
+
+    def process(self, input, pulse):
+        # pulses  = dict(input, pulse)
+        self.last_input = pulse
+        #self.last_inputs[input] = pulse
+
+
+    def send(self):
+        #print(f'In {self.name}, to send *{self.to_send}* to {self.outputs} {self.will_send}')
+        #to_send = self.last_input
+        for output in self.outputs:
+            print('send:' + self.name+' -'+self.last_input+'-> '+output)
+            modules[output].process(self.name, self.last_input)
+        for output in self.outputs:
+            print('*')
+            num_of_pulses[self.last_input] += 1
+            modules[output].send()
+        #for output in self.outputs:
+         #   modules[output].send()
+
+# Jag kanske ska hantera alla skickade pulser i nån sorts kö.
+# Då behöver jag inte hantera det i klasserna. Det låter som att det blir enklare
+
+
 num_of_pulses = {'high': 0, 'low': 0}
 modules = {}
 for d in data:
-    module = Module(d)
-    modules[module.name] = module
-modules['output'] = Module(d)
-modules['output'].type = 'output'
+    typename, outputs = d.replace(' ', '').split('->')
+    outputs = outputs.split(',')
+    if typename[0] == 'b':
+        name = typename
+        modules[name] = Broadcast(name, outputs)
+        #self.name = typename
+        #self.state = None
+        #self.last_received = None
+        # self.will_send = True
+    elif typename[0] == '%':
+        name = typename[1:]
+        modules[name] = FlipFlop(name, outputs[0])
+    elif typename[0] == '&':
+        name = typename[1:]
+        modules[name] = Conjunction(name, outputs[0])
+#modules['output'] = Här måste jag få till en lösning för test2.
 
 for module_name, module in modules.items():
-    for output in module.outputs:
-        modules[output].add_input(module_name)
+    for output in module.get_outputs():
+        modules[output].add_input_module(module_name)
 
 #modules['broadcaster'].inputs.append('button')
 
 modules['broadcaster'].process('button','low')
 modules['broadcaster'].send()
-print('----')
-modules['broadcaster'].process('button','low')
-modules['broadcaster'].send()
-print('----')
-modules['broadcaster'].process('button','low')
-modules['broadcaster'].send()
-print('----')
-modules['broadcaster'].process('button','low')
-modules['broadcaster'].send()
+
+#print('----')
+#modules['broadcaster'].process('button','low')
+#modules['broadcaster'].send()
+#print('----')
+#modules['broadcaster'].process('button','low')
+#modules['broadcaster'].send()
+#print('----')
+#modules['broadcaster'].process('button','low')
+#modules['broadcaster'].send()
